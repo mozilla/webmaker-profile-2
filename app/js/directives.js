81224df4fb212a131e75daf249f4e70b7a5b6309
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-04-18 14:33:22 -0700

    highlighting proper section tab on load

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,8 +1,9 @@
 angular.module('wmProfile.directives', [])
   .directive('wmpToggleGroup', ['jQuery',
+    // Put an "active" class on only the last clicked element in a group
     function ($) {
       return {
-        restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+        restrict: 'A',
         link: function ($scope, el, attrs) {
           var elTriggers = $(el).children();
 
@@ -15,7 +16,28 @@ angular.module('wmProfile.directives', [])
       };
     }
   ])
+  .directive('wmpActivateChild', ['jQuery',
+    // Put "active" class on child based on starting URL
+    function ($) {
+      return {
+        restrict: 'AE',
+        link: function ($scope, el, attrs, controller) {
+          var elChildren = $(el).children();
+
+          for (var i = 0, ii = elChildren.length; i < ii; i += 1) {
+            var view = elChildren.eq(i).attr('href').split('#!/{{ WMP.username }}/')[1];
+
+            if (window.location.hash.match(view)) {
+              elChildren.eq(i).addClass('active');
+              break;
+            }
+          }
+        }
+      };
+    }
+  ])
   .directive('wmpSortFilterBar', ['jQuery',
+    // Pull in sorting and filtering commands from a button group and put choices on scope
     function ($) {
       return {
         restrict: 'AE',
@@ -23,10 +45,6 @@ angular.module('wmProfile.directives', [])
           var elTriggers = $(el).children();
 
           elTriggers.on('click', function (event) {
-            // // Set active class only on current button choice
-            // elTriggers.filter('.active').removeClass('active');
-            // $(this).addClass('active');
-
             if ($(this).data('content-filter')) {
               scope.filterBy = {
                 contentType: 'application/x-' + $(this).data('content-filter')
@@ -44,13 +62,6 @@ angular.module('wmProfile.directives', [])
             scope.$apply();
           });
         }
-      };
-    }
-  ])
-  .directive('appVersion', ['version',
-    function (version) {
-      return function (scope, elm, attrs) {
-        elm.text(version);
       };
     }
   ]);
