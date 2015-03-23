61f951efdbecd289ad4e79dcc3344660b9bc600d
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-03-23 15:47:43 -0700

    take screenshots on failures

--- test/phantom.js
+++ test/phantom.js
@@ -100,8 +100,12 @@ var tests = [
 ];
 
 // Decorate Test instances with an `onComplete` method to trigger the reporter and runner
-tests.forEach(function (test) {
+tests.forEach(function (test, index) {
   test.onComplete = function (didPass) {
+    if (!didPass) {
+      this.page.render('test/screenshots/' + index + '-FAIL.png');
+    }
+
     reporter.report(this.description, didPass);
     runner.onTestComplete();
   };
